f586f8ebec8a9a4a30df895316bf7a63444e7ec7
Bharat Bhargava <f20170025@pilani.bits-pilani.ac.in>
2020-02-20 20:23:13 +0530

    enum to string

---
+++ parser/enum_to_string.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+
+int main(int c, char *argv[])
+{
+    FILE *fp = fopen(argv[1], "r");
+    if(fp == NULL)
+        return 0;
+    char ch;
+    FILE *fp2 = fopen("strings_of_enum.txt", "w");
+    fputc('"',fp2);
+    while((ch = fgetc(fp)) != EOF)
+    {
+        if(ch == ',')
+        {
+            fputc('"',fp2);
+            fputc(ch,fp2);
+            fputc('"',fp2);
+            continue;
+        }
+        fputc(ch,fp2);
+    }
+    fputc('"',fp2);
+    fclose(fp);
+    fclose(fp2);
+    return 0;
+}
