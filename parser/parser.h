342d7ef113e3ed05ba1ce9742b5d7fdffe18737e
Satvik Golechha <f20170117@pilani.bits-pilani.ac.in>
2020-02-21 17:41:43 +0530

    add 2 enums not 1

--- parser/parser.h
+++ parser/parser.h
@@ -8,8 +8,9 @@
 #define MAX_NUM_NON_TERMINALS 100  // number of non-terminals
 #include "bool.h"
 
-/*
-enum terminals  {   AND,ARRAY,ASSIGNOP,BC,BO,BOOLEAN,BREAK,
+
+enum terminals  {   
+                    AND,ARRAY,ASSIGNOP,BC,BO,BOOLEAN,BREAK,
                     CASE,COLON,COMMA,DECLARE,DEF,DEFAULT,DIV,
                     DRIVER,DRIVERDEF,DRIVERENDDEF,END,ENDDEF,EQ,FALSE,
                     FOR,GE,GET_VALUE,GT,IN,INPUT,INTEGER,LE,LT,MINUS,
@@ -18,8 +19,22 @@ enum terminals  {   AND,ARRAY,ASSIGNOP,BC,BO,BOOLEAN,BREAK,
                     SQBC,SQBO,START,SWITCH,TAKES,TRUE,USE,WHILE,WITH
                 };
 
+char *terminals_array[]   {
+                            "arithmeticExpr","arithmeticExpr_lr","arithmeticOrBooleanExpression",
+                            "assignmentStmt","booleanConst","caseStmt","caseStmts","conditionalStmt",
+                            "datatype","declareStmt","default_nt","driverModule","expression",
+                            "factor","idList","idList_lr","index_nt","input_plist","input_plist_lr",
+                            "ioStmt","iterativeStmt","logicalOp","lvalueARRStmt","lvalueIDStmt",
+                            "module","moduleDeclaration","moduleDeclarations","moduleDef",
+                            "moduleReuseStmt","one_more_opt","op1","op2","opt_expr","opt_expr_lr",
+                            "optional","otherModules","output_plist","output_plist_lr","printOpt",
+                            "program","range","rangeArr","relationalOp","ret","simpleStmt","statement",
+                            "statements","term","term_lr","type","unary_opt","unaryExpression",
+                            "value","var","whichId","whichStmt"
+                        }
 
-enum non_terminals {    arithmeticExpr,arithmeticExpr_lr,arithmeticOrBooleanExpression,
+enum non_terminals {    
+                        arithmeticExpr,arithmeticExpr_lr,arithmeticOrBooleanExpression,
                         assignmentStmt,booleanConst,caseStmt,caseStmts,conditionalStmt,
                         datatype,declareStmt,default_nt,driverModule,expression,factor,
                         idList,idList_lr,index_nt,input_plist,input_plist_lr,
@@ -31,8 +46,18 @@ enum non_terminals {    arithmeticExpr,arithmeticExpr_lr,arithmeticOrBooleanExpr
                         ,statements,term,term_lr,type,unary_opt,unaryExpression,value,
                         var,whichId,whichStmt
                     };
-*/
 
+char *non_terminals_array[] =   {
+                                    "AND","ARRAY","ASSIGNOP","BC","BO","BOOLEAN","BREAK","CASE","COLON",
+                                    "COMMA","DECLARE","DEF","DEFAULT", "DOLLAR", "DIV","DRIVER","DRIVERDEF",
+                                    "DRIVERENDDEF","END","ENDDEF", "EPS", "EQ","FALSE","FOR","GE","GET_VALUE",
+                                    "GT", "ID","IN","INPUT","INTEGER","LE","LT","MINUS","MODULE","MUL","NE",
+                                    "NUM","OF","OR","PARAMETERS","PLUS","PRINT","PROGRAM","RANGEOP","REAL",
+                                    "RETURNS","RNUM","SEMICOL","SQBC","SQBO","START","SWITCH","TAKES",
+                                    "TRUE","USE","WHILE","WITH"
+                                }
+
+/*
 enum variables {    arithmeticExpr,arithmeticExpr_lr,
                     arithmeticOrBooleanExpression,assignmentStmt,booleanConst,caseStmt,
                     caseStmts,conditionalStmt,datatype,declareStmt,default_nt,driverModule,
@@ -74,6 +99,7 @@ char *variables_array[] = {
                             "TRUE","USE","WHILE","WITH"
                             
                         };
+*/
 
 typedef struct first_follow_node
 {
