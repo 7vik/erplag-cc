80861490c471c066bcd58f87ad1146ddfe1c800d
Satvik Golechha <f20170117@pilani.bits-pilani.ac.in>
2020-02-21 23:42:31 +0530

    create final parser prototype

--- parser/final_parser.c
+++ parser/final_parser.c
@@ -10,6 +10,45 @@
 
 #define GRAMMAR_START_SYMBOL program
 
+PARSE_TREE *create_new_node (TREE_NODE *data)
+{
+    // creates a new leaf node storing data, and returns the node
+    PARSE_TREE *new = (PARSE_TREE *) malloc(sizeof(PARSE_TREE));
+    new->data = data;
+    new->num_of_kids = 0;
+    int end = N_ARY_LIM;
+    while(--end >= 0) new->kids[end] = NULL;
+    return new;
+}
+
+// recursive
+void print_parse_tree(PARSE_TREE *tree, char *out_file_name)
+{
+    // prints the given parse tree in an output file in inorder traversal
+    // for an n-ary tree, inorder traversal means:
+    //          leftmost kid, then pop, then other kids
+    FILE *f = fopen(out_file_name, "w");
+    if (tree == NULL)   
+        return;         // print nothing
+    else
+    {
+        print_parse_tree(tree->kids[0], out_file_name);
+        fprintf(f, "%15s\t%15u\t%15s\t%15s\t%15s\t%15d\t%15s\n",
+                    tree->data->lexeme,
+                    tree->data->line,
+                    tree->data->token_name,
+                    tree->data->value_if_number,
+                    tree->data->parent_node_symbol,
+                    tree->data->is_leaf_node,
+                    tree->data->node_symbol
+                    );
+        int temp = tree->num_of_kids;
+        while (--temp)
+            print_parse_tree(tree->kids[temp], out_file_name);
+    }
+    return;
+}
+
 // returns 0 if the token is non-terminal
 int is_terminal(char* variable)
 {
@@ -420,6 +459,15 @@ first_follow* get_first_follow_table(GRAMMAR* grammar)
     return construct_first_follow_set(grammar, grammar->num_rules);
 }
 
+void create_parse_table(first_follow *f, TABLE *t)
+{
+    return;
+}
+
+void parse(char *filename, TABLE *table, PARSE_TREE *tree)
+{
+    return;
+}
 
 int main()
 {
