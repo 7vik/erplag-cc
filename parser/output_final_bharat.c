9b9f674bc02f907e9c4b699ef4c4ca264b21aa49
ayushjain1144 <ayushjain1144@gmail.com>
2020-02-22 19:08:23 +0530

    for bharat's ref

---
+++ parser/output_final_bharat.c
@@ -0,0 +1,312 @@
+DECLARE DEF DRIVERDEF 
+Follow set: 
+			***********************			
+DECLARE 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+DECLARE 
+Follow set: 
+			***********************			
+DEF 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+DRIVERDEF 
+Follow set: 
+			***********************			
+DEF 
+Follow set: 
+			***********************			
+RETURNS 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+COMMA 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+ID 
+Follow set: 
+			***********************			
+COMMA 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+ID 
+Follow set: 
+			***********************			
+INTEGER 
+Follow set: 
+			***********************			
+REAL 
+Follow set: 
+			***********************			
+BOOLEAN 
+Follow set: 
+			***********************			
+ARRAY 
+Follow set: 
+			***********************			
+ID NUM 
+Follow set: 
+			***********************			
+INTEGER 
+Follow set: 
+			***********************			
+REAL 
+Follow set: 
+			***********************			
+BOOLEAN 
+Follow set: 
+			***********************			
+START 
+Follow set: 
+			***********************			
+DECLARE FOR GET_VALUE ID PRINT SQBO SWITCH USE WHILE 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+GET_VALUE PRINT 
+Follow set: 
+			***********************			
+ID SQBO USE 
+Follow set: 
+			***********************			
+DECLARE 
+Follow set: 
+			***********************			
+SWITCH 
+Follow set: 
+			***********************			
+FOR WHILE 
+Follow set: 
+			***********************			
+GET_VALUE 
+Follow set: 
+			***********************			
+PRINT 
+Follow set: 
+			***********************			
+ID 
+Follow set: 
+			***********************			
+NUM 
+Follow set: 
+			***********************			
+RNUM 
+Follow set: 
+			***********************			
+SQBO 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+FALSE TRUE 
+Follow set: 
+			***********************			
+ID NUM RNUM 
+Follow set: 
+			***********************			
+ID 
+Follow set: 
+			***********************			
+SQBO USE 
+Follow set: 
+			***********************			
+ID 
+Follow set: 
+			***********************			
+ASSIGNOP 
+Follow set: 
+			***********************			
+SQBO 
+Follow set: 
+			***********************			
+ASSIGNOP 
+Follow set: 
+			***********************			
+SQBO 
+Follow set: 
+			***********************			
+NUM 
+Follow set: 
+			***********************			
+ID 
+Follow set: 
+			***********************			
+SQBO USE 
+Follow set: 
+			***********************			
+SQBO 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+COMMA 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+ID 
+Follow set: 
+			***********************			
+MINUS PLUS 
+Follow set: 
+			***********************			
+BO FALSE ID NUM RNUM TRUE 
+Follow set: 
+			***********************			
+MINUS 
+Follow set: 
+			***********************			
+PLUS 
+Follow set: 
+			***********************			
+BO 
+Follow set: 
+			***********************			
+ID NUM RNUM 
+Follow set: 
+			***********************			
+BO FALSE ID NUM RNUM TRUE 
+Follow set: 
+			***********************			
+AND OR 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+BO ID NUM RNUM 
+Follow set: 
+			***********************			
+FALSE TRUE 
+Follow set: 
+			***********************			
+EQ GE GT LE LT NE 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+TRUE 
+Follow set: 
+			***********************			
+FALSE 
+Follow set: 
+			***********************			
+MINUS PLUS 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+BO ID NUM RNUM 
+Follow set: 
+			***********************			
+DIV MUL 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+BO ID NUM RNUM 
+Follow set: 
+			***********************			
+BO 
+Follow set: 
+			***********************			
+ID NUM RNUM 
+Follow set: 
+			***********************			
+PLUS 
+Follow set: 
+			***********************			
+MINUS 
+Follow set: 
+			***********************			
+MUL 
+Follow set: 
+			***********************			
+DIV 
+Follow set: 
+			***********************			
+AND 
+Follow set: 
+			***********************			
+OR 
+Follow set: 
+			***********************			
+LT 
+Follow set: 
+			***********************			
+LE 
+Follow set: 
+			***********************			
+GT 
+Follow set: 
+			***********************			
+GE 
+Follow set: 
+			***********************			
+EQ 
+Follow set: 
+			***********************			
+NE 
+Follow set: 
+			***********************			
+DECLARE 
+Follow set: 
+			***********************			
+NUM 
+Follow set: 
+			***********************			
+TRUE 
+Follow set: 
+			***********************			
+FALSE 
+Follow set: 
+			***********************			
+CASE 
+Follow set: 
+			***********************			
+CASE 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+DEFAULT 
+Follow set: 
+			***********************			
+EPS 
+Follow set: 
+			***********************			
+SWITCH 
+Follow set: 
+			***********************			
+NUM 
+Follow set: 
+			***********************			
+FOR 
+Follow set: 
+			***********************			
+WHILE 
+Follow set: 
+			***********************			
