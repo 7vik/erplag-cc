7761ad2a0f455a7d648cdcdeda094211cc72fcd3
Satvik Golechha <f20170117@pilani.bits-pilani.ac.in>
2020-02-24 16:00:19 +0530

    thotha chana baaje ghana

---
+++ Group_08/bool.h
@@ -0,0 +1,16 @@
+#ifndef __BOOL
+#define __BOOL
+#define MAX_BOOL_ARRAY_SIZE  150             // maximum allowed length of boolean array
+
+
+#include <stdbool.h>
+
+void initialize_bool_array(bool* arr);
+bool* or(bool * arr1, bool* arr2);
+bool* and(bool * arr1, bool* arr2);
+bool* xor(bool * arr1, bool* arr2);
+bool* not(bool * arr1);
+void print_bool(bool* arr, int size);
+void or_and_store(bool * arr1, bool* arr2);
+
+#endif
