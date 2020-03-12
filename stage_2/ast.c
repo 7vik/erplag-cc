fe352fc9ff92904de1a0fd9d300c48dc1bc8d383
Satvik Golechha <f20170117@pilani.bits-pilani.ac.in>
2020-03-12 16:38:29 +0530

    The one where we don't know what to ask

---
+++ stage_2/ast.c
@@ -0,0 +1,36 @@
+//                  ,-.                         Group 08                                                    //
+//          ,      ( {o\                        Satvik Golechha :: 2017A7PS0117P                           ///
+//          {`"=,___) (`~                       Bharat Bhargava :: 2017A7PS0025P                          ////
+//           \  ,_.-   )                        Ayush      Jain :: 2017A7PS0093P                         /////
+//      ~^~^~^`- ~^ ~^ '~^~^~^~                                                                         //////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+#include "parser.h"
+
+/* 
+Algorithm:  return: abstract syntax tree 
+            from:   concrete parse tree obtained from the parser
+
+1. For each rule in the grammar, define semantic rules for value and type attributes.
+2. Terminals (leaf nodes) do not have any inherited attributes, so directly fill them.
+
+
+
+Doubts:
+1. Do we need to store line numbers in the AST node to yield semantic errors?
+2. Except 'type' and 'value' fields, what else is required?
+3. What are the 'type' and 'value' for nodes for which they are not defined?
+4. Algorithm for AST: order of evaluation (postorder for syn attributes)
+5. If a node isn't a leafnode, how do we know which grammar rule we used?
+6. Parser stack implementation and dependancy graphs
+
+
+*/
+
+int main()          // driver
+{
+    return 0;
+}
