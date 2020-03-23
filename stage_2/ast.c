5e0b78a94976da91336176d37a9d47ef07d7a386
Satvik Golechha <f20170117@pilani.bits-pilani.ac.in>
2020-03-23 17:27:28 +0530

    add rule 10

--- stage_2/ast.c
+++ stage_2/ast.c
@@ -248,9 +248,19 @@ return node;
 break;
 }
 
-case(10):
+case(10)://ret -> EPS
+
 {
         //To do
+        int certificate = string_to_enum(root->data->lexeme);
+
+astNode* node = make_ASTnode(certificate);
+                node->tree_node = root->kids[0]->data;
+
+node->is_leaf = 1;
+
+return node;
+        
 }
 
 case(11):
