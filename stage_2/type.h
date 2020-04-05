aa7c48af28fd5bb8aecafbc920c836b509a4ae0e
Satvik Golechha <f20170117@pilani.bits-pilani.ac.in>
2020-04-05 12:33:46 +0530

    add type.h

---
+++ stage_2/type.h
@@ -0,0 +1,31 @@
+#ifndef __TYPE
+#define __TYPE
+
+// type data for an array
+typedef struct ARRAY_TYPE_DATA
+{
+    // array[begin..end] of simple
+    unsigned int begin;
+    unsigned int end;
+    unsigned short int simple;                // 1,2,3 for integer, real, boolean
+    // changes here if she allows for nested arrays
+} ARRAY_TYPE_DATA;
+
+// // type data for a function
+// typedef struct FuNCTION_TyPE_DaTA
+// {
+//     // a function type has inputs and outputs (as an array)
+//     unsigned int num_in, num_out;                        // number of inputs/outputs
+//     struct symbol_data *inputs[FUNC_PARAMS];                     // everything else NULL, only for type of parameters with name
+//     struct symbol_data *outputs[FUNC_PARAMS];
+// } FUNCTION_TYPE_DATA;
+
+// abstract structure to store any given type (insert a good type type pun)
+typedef struct TYPE_INFO
+{
+    unsigned short int simple;                   // 1,2,3,4 for integer, real, boolean, array
+    ARRAY_TYPE_DATA *arrtype;                   // if array, else NULL
+    // FUNCTION_TYPE_DATA *functype;              // if function, else NULL
+} TYPE;
+
+#endif
