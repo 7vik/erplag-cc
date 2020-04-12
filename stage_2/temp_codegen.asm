90de73daef9be8de8528e1589542938f731e8800
ayushjain1144 <ayushjain1144@gmail.com>
2020-04-12 13:35:12 +0530

    commented asm program -> temp_codegen

--- stage_2/temp_codegen.asm
+++ stage_2/temp_codegen.asm
@@ -1,14 +1,47 @@
-        global _start
-        extern printf
-        extern scanf
-        extern puts
+;     nasm -felf64 temp_codegen.asm
+;     gcc temp_codegen.o
+;     gcc ./a.out
 
         section .data
 
-        section .text
+int_inMsg:    db        "Enter an integer value" , 10, 0 ;10 for new line
+real_inMsg:   db        "Enter a real value", 10
+bool_inMsg:   db        "Enter a boolean value", 10
+arr_inMsg:    db        "Enter %d elements for array range %d to %d"
 
-_start: 
 
-        section .data
+        section .bss
+var1:         resd      1
 
-temp1:  dq      0   
+
+global main
+extern printf
+extern scanf
+extern puts
+extern exit
+
+section .text
+
+main: 
+
+        push rbp ;setup stack
+
+        ;printf("Enter blah blah\n");
+
+        mov rdi, int_inMsg ;first argument
+        call printf
+
+        ;take input from the user
+        ;scanf("%d", &var1);
+
+        
+
+        ; return
+        pop rbp ;restore stack
+        mov rax, 0 ;normal exit
+        ret
+
+
+
+
+
