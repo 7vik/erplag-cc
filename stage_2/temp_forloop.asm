b36a478d8f710c8de0eb8f15a159295df04386e7
Bharat Bhargava <f20170025@pilani.bits-pilani.ac.in>
2020-04-14 15:58:56 +0530

    Working correct imo

---
+++ stage_2/temp_forloop.asm
@@ -0,0 +1,55 @@
+section .bss
+	start:	resd	1
+	end:	resd	1
+
+section .data
+	print_start db "Enter the start index: ",0
+	print_end db "Enter the end index: ",0
+	inp_start_end db "%d",0
+	print_string db "Hello World!",10,0
+	 
+section .text
+	global main
+	extern printf
+	extern scanf
+
+main:
+	push rbp
+	xor rax, rax
+	mov rdi, print_start
+	call printf
+	
+	xor rax, rax
+	mov rdi, inp_start_end
+	mov rsi, start
+	call scanf
+	
+	xor rax, rax
+	mov rdi, print_end
+	call printf
+	
+	xor rax, rax
+	mov rdi, inp_start_end
+	mov rsi, end
+	call scanf
+	
+	mov ebx, [start]
+	mov ecx, [end]
+	sub ecx, ebx
+	inc ecx
+	
+loop:	xor rax, rax
+	mov rdi, print_string
+
+	push rcx
+	push rbx
+	call printf
+	pop rbx
+	pop rcx
+
+	dec ecx
+	cmp ecx, 0
+	jne loop
+	
+	pop rbp
+	ret 
