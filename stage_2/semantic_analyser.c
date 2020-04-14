e8f49f1e7c3319fd5be16bcd6e087b96179dee47
Bharat Bhargava <f20170025@pilani.bits-pilani.ac.in>
2020-04-14 21:36:05 +0530

    Kya bakwaas hai

--- stage_2/semantic_analyser.c
+++ stage_2/semantic_analyser.c
@@ -37,16 +37,25 @@ void semantic_analyser(astNode* root, GST* global_st)
         assert(root->node_marker == program);
         
         astNode* temp = root->child;
-        check_moduleDeclarations_semantic(temp, global_st);
-        temp = temp->sibling;
-
-        check_otherModules_semantic(temp, global_st);
-        temp = temp->sibling;
-
-        check_driverModule_semantic(temp, global_st);
-        temp = temp->sibling;
-
-        check_otherModules_semantic(temp, global_st);
+        //printf("%s\n",variables_array[temp->node_marker]);
+        if(temp->node_marker == moduleDeclarations)
+        {
+            printf("%s\n",variables_array[temp->node_marker]);
+            check_moduleDeclarations_semantic(temp, global_st);
+            temp = temp->sibling;
+        }
+        if(temp->node_marker == otherModules)
+        {
+            check_otherModules_semantic(temp, global_st);
+            temp = temp->sibling;
+        }
+        if(temp->node_marker == driverModule)
+        {
+            check_driverModule_semantic(temp, global_st);
+            temp = temp->sibling;
+        }
+        if(temp->node_marker == otherModules)
+            check_otherModules_semantic(temp, global_st);
 
         return;
 
@@ -61,6 +70,7 @@ void semantic_analyser(astNode* root, GST* global_st)
 
 void check_moduleDeclarations_semantic(astNode* root, GST* global_st)
 {
+    //printf("Entered this zone");
     if(root->node_marker == EPS)
         return;
 
@@ -84,6 +94,7 @@ void check_moduleDeclarations_semantic(astNode* root, GST* global_st)
         
         temp = temp->sibling;
     }
+    //printf("We're successful!");
     return;
 }
 
@@ -550,4 +561,48 @@ void check_var_semantic(astNode* root, ID_SYMBOL_TABLE* id_table)
         
     }
     return;
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc != 3)
+    {
+        printf("Invalid argument count. Expected 3 arguments as in './executable testcase parse_outfile'.");
+        printf("\nAborting Execution!!\n");
+        exit(2);
+    }
+
+    FILE* test_fp = fopen(argv[1], "r");
+    FILE* test_parse_fp = fopen(argv[2], "w");
+    populate_ht(hash_table, KEYWORDS_FILE);
+    int line_count = 1;
+    TWIN_BUFFER *twin_buff = (TWIN_BUFFER *) malloc(sizeof(TWIN_BUFFER));
+    init_buffer(test_fp, twin_buff);
+    GRAMMAR* grammar = generate_grammar();
+    first_follow *ff = get_first_follow_table(grammar);
+    TABLE *parse_table = (TABLE *) malloc(sizeof(TABLE));
+    create_parse_table(ff, parse_table, grammar);
+    STACK *stack = NULL;
+    PARSE_TREE *tree;
+    parse(grammar, test_fp, parse_table, &tree, stack, twin_buff, &line_count);
+    fprintf(test_parse_fp, "%20s\t%20s\t%20s\t%20s\t%20s\t%20s\t%20s%20s\n\n", "LEXEME", "LINE_NO", "VALUE (if num)", "TOKENAME",  "PARENT", "IS LEAF?", "NODE SYMBOL", "RULE_NUMBER");
+    print_parse_tree(tree, test_parse_fp);
+    print_parse_tree_json(tree, "output_parse_tree.json");
+    printf("Printed Parse Tree in file '%s'.\n", argv[2]);
+    astNode* ast_root = buildAST(tree);
+    print_ast_json(ast_root, "output_ast_tree.json");
+
+
+    // Test Symbol table
+    GST *st = create_global_st();
+    traverse_the_multiverse(ast_root, st);
+    gst_print(st);
+
+    semantic_analyser(ast_root, st);
+
+    fclose(test_fp);
+    fclose(test_parse_fp);
+    free(twin_buff);
+    free(parse_table);
+    return 0;
 }
