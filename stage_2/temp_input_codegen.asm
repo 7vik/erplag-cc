a43084a98326eb20c8ec58ed07d4bab6521d610e
ayushjain1144 <ayushjain1144@gmail.com>
2020-04-14 16:03:23 +0530

    fixed bugs

--- stage_2/temp_input_codegen.asm
+++ stage_2/temp_input_codegen.asm
@@ -10,16 +10,16 @@ type_real     db        "real", 0
 type_bool     db        "boolean", 0
 intFormat_in  db        "%d", 0
 intFormat_out db        "%d ", 0
-realFormat_in db        "%lf", 10, 0
-realFormat_out db       "%lf", 0
+realFormat_in db        "%E", 0
+realFormat_out db       "%E", 0
 strFormat_in   db       "%s", 0
 strFormat_out   db       "%s", 10, 0
 true_label     db        "true ", 0
 false_label     db        "false ", 0
 arr_outMsg   db        "Printing array: ", 0
 new_line       db       10, 0
-var1         dd        7
-var2         dd        14
+var1         dd        1
+var2         dd        2
 
 
 
@@ -49,7 +49,7 @@ mov r8, [var2]
 mov rsi, r8
 sub rsi, rcx
 mov rdi, arr_inMsg
-mov rdx, type_bool
+mov rdx, type_real
 mov rcx, [var1]
 mov r8, [var2]
 call printf
@@ -61,18 +61,20 @@ xor r13, r13
 mov r12d, [var2]
 mov r13d, [var1]
 sub r12d, r13d
-;mov r12, 5
 xor r13, r13
 
 array_input_loop:
-lea rdi, [intFormat_in]
-lea rsi, [bool_array + r13d * 1]
+lea rdi, [realFormat_in]
+lea rsi, [real_array + r13d * 8]
+mov rax, 1
 call scanf
+
 
 inc r13d
 cmp r13d, r12d
 jne array_input_loop
 pop rbp
+
 
 
 push rbp
@@ -81,27 +83,24 @@ lea rdi, [arr_outMsg]
 call printf
 ; stores the count
 
+
 xor r12, r12
 xor r13, r13
 mov r12d, [var2]
 mov r13d, [var1]
 sub r12d, r13d
-;mov r12, 5
 xor r13, r13
 
+
 array_output_loop:
-lea rdi, [strFormat_in]
-mov sil, [bool_array + r13d * 1]
+lea rdi, [realFormat_out]
+;xor rsi, rsi
+movq xmm0, qword [real_array + r13d * 8]
 
-cmp sil, 0
-jz false
-lea rsi, [true_label]
-jmp print
-
-false: 
-lea rsi, [false_label]
-
-print: 
+ret
+;fld qword [real_array + r13d * 8]
+;fstp qword [rsi]
+mov rax, 1
 call printf
 
 inc r13d
